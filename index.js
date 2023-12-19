f9f2e8f2ba63f9ab260bd3a617a6077c3f0cbe5f
CaetanoMath <matheusjohnny.mjc@gmail.com>
2023-12-19 15:09:44 -0300

    projeto heroi

---
+++ index.js
@@ -0,0 +1,32 @@
+var xp = 10001
+var nome = "Johnny"
+var pronome = "masculino"
+let nivel = " "
+
+if(xp<=1000){nivel = "Ferro"}
+else if(xp>1000 && xp<=2000){nivel = "Bronze"}
+
+else if(xp>2000 && xp<=5000){nivel = "Prata"}
+
+else if(xp> 5000 && xp<=7000){nivel = "Ouro"}
+
+else if(xp> 7000 && xp<=8000){nivel = "Platina"}
+
+else if(xp>8000 && xp<=9000){nivel = "Ascendente"}
+
+else if(xp>9000 && xp<=10000){nivel = "Imortal"}
+
+else if(xp>10000){nivel = "Radiante"}
+
+switch(pronome){
+    case "masculino":
+        console.log("O Herói de nome "+nome+" está no nível de " + nivel+"!")
+        break;
+    case "feminino":
+        console.log("A Heroina de nome "+nome+" está no nível de " + nivel+"!")
+        break;
+    case "neutro":
+        console.log("Heroine de nome "+nome+" está no nível de " + nivel+"!")
+
+}
+
